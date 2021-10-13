be52de529437c2ddc9a0a6463d5a1a88918635d3
stefania sanna <sanna.stefania95@gmail.com>
2021-10-13 21:04:47 +0200

    Esercizio Ricorsione Fattoriale

---
+++ EsercizioFattoriale/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Fattoriale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int fattorialeIterazione = FattorialeIterativo(n);
+            Console.WriteLine($"Il fattoriale {n}! calcolato con iterazione è {fattorialeIterazione}");
+
+            int fattorialeRicorsione = FattorialeRicorsione(n);
+            Console.WriteLine($"Il fattoriale {n}! calcolato con ricorsione è {fattorialeRicorsione}");
+
+        }
+        private static int FattorialeIterativo(int n)
+        {
+            int fattoriale = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fattoriale *= i;
+            }
+            return fattoriale;
+        }
+
+        private static int FattorialeRicorsione(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return FattorialeRicorsione(n - 1) * n;
+            }
+        }
+    }
+}
